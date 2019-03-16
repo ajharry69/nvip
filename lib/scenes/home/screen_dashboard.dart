30d36c8ccb3efa607482ec188f8535707b28ceb0
ajharry69 <ajuongharry35@gmail.com>
2019-03-16 11:30:40 +0300

    Created a home dash-board screen and modified gitignore

---
+++ lib/scenes/home/screen_dashboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DashBoardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => _DashBoardScreenBody();
+}
+
+class _DashBoardScreenBody extends StatefulWidget {
+  @override
+  __DashBoardScreenBodyState createState() => __DashBoardScreenBodyState();
+}
+
+class __DashBoardScreenBodyState extends State<_DashBoardScreenBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
