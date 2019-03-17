643d5f158859118ae6c012a477ea5e464bd4459f
ajharry69 <ajuongharry35@gmail.com>
2019-03-17 21:54:52 +0300

    Custom widget for showing image from network with failure widget shown as an alternative for image loading error

---
+++ lib/widgets/post_image_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomFadeInImageView extends StatefulWidget {
+  final String imageUrl;
+
+  const CustomFadeInImageView({Key key, this.imageUrl}) : super(key: key);
+
+  @override
+  _CustomFadeInImageViewState createState() =>
+      _CustomFadeInImageViewState(imageUrl);
+}
+
+class _CustomFadeInImageViewState extends State<CustomFadeInImageView> {
+  final String imageUrl;
+  final defaultImage = "images/no_image.png";
+  final imageHeight = 194.0;
+
+  _CustomFadeInImageViewState(this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return imageUrl != null
+        ? FadeInImage.assetNetwork(
+            placeholder: defaultImage,
+            image: imageUrl,
+            width: double.maxFinite,
+            height: imageHeight,
+            fit: BoxFit.fill,
+          )
+        : Image.asset(
+            defaultImage,
+            width: double.maxFinite,
+            height: imageHeight,
+            fit: BoxFit.fill,
+          );
+  }
+}
